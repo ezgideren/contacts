ec37dd7d230d96cb08ce8a621a2724ab53305060
ezgideren <ezgi_deren@yahoo.com>
2024-06-29 11:41:32 +0200

    api done

--- backend/main.py
+++ backend/main.py
@@ -29,8 +29,36 @@ def create_contact():
         db.session.commit()
     except Exception as e:
         return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message:" "User created!"}), 201
         
+@app.route("/update_contact/<int:user_id>")
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
 
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+    
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+
+    return jsonify({"message": "User updated."}), 200
+
+app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
+def delete_contact(user_id):
+    contact = Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+    
+    db.sesion.delete(contact)
+    db.session.commit()
+    
+    return jsonify({"message": "User deleted!"}), 200
 
 #if we import this main.py file don't do this otherwise run
 if __name__ == "__main__":
